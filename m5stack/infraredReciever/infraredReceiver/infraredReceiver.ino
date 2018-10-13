c0fd82b8c99826151bf8a416815c8b19037bbe2c
takashi <t.k.mk3.110@gmail.com>
2018-10-13 13:05:13 +0900

    update ir

--- m5stack/infraredReciever/infraredReceiver/infraredReceiver.ino
+++ m5stack/infraredReciever/infraredReceiver/infraredReceiver.ino
@@ -1,54 +1,39 @@
 #include <M5Stack.h>
 
 #define READ_PIN 21
-#define LOW_STATE 0
-#define HIGH_STATE 1
+#define IRDATA_SIZE 512
 
 void setup() {
   M5.begin();
   Serial.begin(115200);
   pinMode(READ_PIN, INPUT);
-
-  Serial.println("Ready to receive");
 }
-
-void waitLow() {
-  while (digitalRead(READ_PIN) == LOW) { }
-}
-
-int waitHigh() {
-  unsigned long start = micros();
-
-  while (digitalRead(READ_PIN) == HIGH) {
-    if (micros() - start > 5000000) {
-      return 1;
-    }
-  }
-
-  return 0;
-}
-
-unsigned long now = micros();
-unsigned long lastStateChangedMicros = micros();
-int state = HIGH_STATE;
 
 void loop() {
-  if (state == LOW_STATE) {
-    waitLow();
-  } else {
-    int ret = waitHigh();
-    if (ret == 1) {
-      Serial.print("\n");
-      return;
+  unsigned long usec, irOffTime;
+  int i, j;
+  unsigned int irdata[IRDATA_SIZE];
+
+  while (digitalRead(READ_PIN) == HIGH);
+  for (i=0; i < IRDATA_SIZE;) {
+    usec = micros();
+    while (digitalRead(READ_PIN) == LOW);
+    irdata[i] = micros() - usec;
+    irdata[++i] = 0;
+    usec = micros();
+    while (digitalRead(READ_PIN) == HIGH) {
+      irOffTime = micros() - usec;
+      if (irOffTime > 65000) goto ir_exit;
     }
+    irdata[i++] = irOffTime;
   }
-  now = micros();
-  Serial.print((now - lastStateChangedMicros) / 10, DEC);
-  Serial.print(",");
-  lastStateChangedMicros = now;
-  if (state == HIGH_STATE) {
-    state = LOW_STATE;
-  } else {
-    state = HIGH_STATE;
+  
+ir_exit:
+
+  for (j=0; j < i; j++) {
+    Serial.print(j % 2 == 0 ? "ON  " : "OFF ");
+    Serial.print(irdata[j], DEC);
+    Serial.print("\n");
   }
+  Serial.print("\n");
 }
