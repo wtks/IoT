ef3088d209958d162cecf7deed211ed325bd9b81
takashi <t.k.mk3.110@gmail.com>
2018-10-18 19:54:13 +0900

    a75c4269 service

--- raspberrypi/A75C4269/go.mod
+++ raspberrypi/A75C4269/go.mod
@@ -3,4 +3,6 @@ module A75C4269
 require (
 	github.com/djthorpe/gopi v1.0.20
 	github.com/djthorpe/gopi-hw v1.0.5
+	github.com/eclipse/paho.mqtt.golang v1.1.1
+	github.com/wtks/A75C4269 v0.1.1
 )
--- raspberrypi/A75C4269/main.go
+++ raspberrypi/A75C4269/main.go
@@ -1,136 +1,69 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
 	"github.com/djthorpe/gopi"
 	_ "github.com/djthorpe/gopi-hw/sys/lirc"
 	_ "github.com/djthorpe/gopi/sys/logger"
+	"github.com/eclipse/paho.mqtt.golang"
+	"github.com/wtks/A75C4269"
+	"log"
 	"os"
 )
 
 const (
-	T           = 445
-	T8          = T * 8
-	T4          = T * 4
-	T3          = T * 3
-	TracerSpace = T * 20
+	ClientID = "rpizerow_aircon"
+	SubTopic = "/aircon/action"
+	PubTopic = "/aircon/state"
 )
 
-var (
-	CallSign = []byte{0x02, 0x20, 0x0E, 0x04, 0x00, 0x00, 0x00, 0x06}
-)
+var MQTTHost = os.Getenv("MQTT_HOST")
 
 func main() {
+	// init mqtt client
+	mqttOpt := mqtt.NewClientOptions()
+	mqttOpt.AddBroker(fmt.Sprintf("tcp://%s:1883", MQTTHost))
+	mqttOpt.SetClientID(ClientID)
+
+	client := mqtt.NewClient(mqttOpt)
+	defer client.Disconnect(250)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		log.Fatal(token.Error())
+	}
+
 	config := gopi.NewAppConfig("lirc")
-	config.AppFlags.FlagBool("on", false, "power on")
-	config.AppFlags.FlagString("mode", "cooler", "cooler or heater or dehumidifier")
-	config.AppFlags.FlagUint("t", 28, "16-30 ℃")
 
 	os.Exit(gopi.CommandLineTool(config, func(app *gopi.AppInstance, done chan<- struct{}) error {
 		if app.LIRC == nil {
 			return errors.New("missing LIRC module")
 		}
 
-		var d []uint32
-		d = append(d, appendTracerSpace(convertRawSignal(CallSign))...)
+		token := client.Subscribe(SubTopic, 0, func(client mqtt.Client, msg mqtt.Message) {
+			c := A75C4269.Controller{}
+			if err := json.Unmarshal(msg.Payload(), &c); err != nil {
+				app.Logger.Error(err.Error())
+				return
+			}
 
-		r := Remote{}
+			if err := app.LIRC.PulseSend(c.GetRawSignal()); err != nil {
+				app.Logger.Error(err.Error())
+				return
+			}
 
-		on, _ := app.AppFlags.GetBool("on")
-		if on {
-			r.Power = PowerOn
-		} else {
-			r.Power = PowerOff
-		}
+			token := client.Publish(PubTopic, 0, false, string(msg.Payload()))
+			if token.Wait() && token.Error() != nil {
+				app.Logger.Error(token.Error().Error())
+				return
+			}
+		})
 
-		mode, _ := app.AppFlags.GetString("mode")
-		switch mode {
-		case "cooler":
-			r.Mode = ModeCooler
-		case "heater":
-			r.Mode = ModeHeater
-		case "dehumidifier":
-			r.Mode = ModeDehumidifier
-		default:
-			r.Mode = ModeCooler
-		}
-
-		t, _ := app.AppFlags.GetUint("t")
-		r.PresetTemp = t
-
-		d = append(d, convertRawSignal(r.GetSignalBytes())...)
-
-		if err := app.LIRC.PulseSend(d); err != nil {
-			return err
+		if token.Wait() && token.Error() != nil {
+			return token.Error()
 		}
 
 		done <- gopi.DONE
 		return nil
 	}))
-}
-
-func convertRawSignal(bytes []byte) []uint32 {
-	var seq []uint32
-
-	// Leader
-	seq = append(seq, T8, T4)
-
-	// Customer Code 1, 2
-	seq = appendByte(seq, bytes[0])
-	seq = appendByte(seq, bytes[1])
-
-	// Parity & Data0
-	var i uint
-	for i = 4; i < 8; i++ {
-		if refBit(bytes[2], i) == 1 {
-			seq = appendBit1(seq)
-		} else {
-			seq = appendBit0(seq)
-		}
-	}
-	for i = 0; i < 4; i++ {
-		if refBit(bytes[2], i) == 1 {
-			seq = appendBit1(seq)
-		} else {
-			seq = appendBit0(seq)
-		}
-	}
-
-	// DataN
-	for i := 3; i < len(bytes); i++ {
-		seq = appendByte(seq, bytes[i])
-	}
-
-	// Tracer
-	seq = append(seq, T)
-
-	return seq
-}
-
-func appendTracerSpace(seq []uint32) []uint32 {
-	return append(seq, TracerSpace)
-}
-
-func appendBit0(seq []uint32) []uint32 {
-	return append(seq, T, T)
-}
-
-func appendBit1(seq []uint32) []uint32 {
-	return append(seq, T, T3)
-}
-
-func refBit(i byte, b uint) byte {
-	return (i >> b) & 1
-}
-
-func appendByte(seq []uint32, b byte) []uint32 {
-	var i uint
-	for i = 0; i < 8; i++ {
-		if refBit(b, i) == 1 {
-			seq = appendBit1(seq)
-		} else {
-			seq = appendBit0(seq)
-		}
-	}
-	return seq
 }
